0ce3dcc7761b86484e96e4490b5cfab56c2160b6
veeresh-git <veereshv537@gmail.com>
2020-09-24 17:54:10 +0530

    Updates

---
+++ precache-manifest.3e53bcb2c1952ce47c1016dab143750e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94163fbb4cc92192e0475f9ccdff22cd",
+    "url": "/calculator2/index.html"
+  },
+  {
+    "revision": "f606c64983973f7e6ae2",
+    "url": "/calculator2/static/css/main.8f1bd34c.chunk.css"
+  },
+  {
+    "revision": "3f1c0ab92450b4f0e548",
+    "url": "/calculator2/static/js/2.0717c62d.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator2/static/js/2.0717c62d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f606c64983973f7e6ae2",
+    "url": "/calculator2/static/js/main.ef055528.chunk.js"
+  },
+  {
+    "revision": "2ad6a549a661ec59de99",
+    "url": "/calculator2/static/js/runtime-main.3d3e9646.js"
+  }
+]);
